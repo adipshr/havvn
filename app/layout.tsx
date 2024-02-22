a8f633d5512953a7fb968389c03eeea94d5b1733
adipshr <adi.pshr@gmail.com>
2024-02-22 22:30:51 +0530

    feat:signin button UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,16 @@ import { Inter, Nunito, Montserrat } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 
+import Modal from "./components/modals/Modal";
+
 const inter = Inter({ subsets: ["latin"] });
 const nunito = Nunito({ subsets: ["latin"] });
 const montserrat = Montserrat({ subsets: ["latin"] });
+
+interface Metadata {
+  title: any;
+  description: any;
+}
 
 export const metadata: Metadata = {
   title: "Havvn",
@@ -19,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={montserrat.className}>
+        <Modal isOpen="true" title="Welcome to Havvn" />
         <Navbar />
         {children}
       </body>
